4f8845a27185b4b65ac635d0948a760bcc66c661
Niels Christian <nielschristiankielland@gmail.com>
2017-05-11 17:49:21 -0700

    pOOP, and Flask

---
+++ Python/flask_fundamentals/counter/server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = "iAmSecret"
+
+@app.route('/')
+def index():
+    if 'counter' not in session:
+        session['counter'] = 0
+    else:
+        session['counter'] += 1
+    return render_template('index.html')
+
+@app.route('/two')#, methods=['GET'])
+def clickTwo():
+    session['counter'] += 1
+    return redirect('/')
+
+@app.route('/reset') #, methods=['GET'])
+def reset():
+    session.clear()
+    return redirect('/')
+
+
+app.run(debug=True)
